efc0773fb0ad2632bc3a5fbdb1051706fc560b13
joestr <strasser999@gmail.com>
2019-01-08 08:06:29 +0100

    updated to version 0.1.2; get rid off class fields array list;

--- src/main/java/xyz/joestr/dbwrapper/DatabaseWrapable.java
+++ src/main/java/xyz/joestr/dbwrapper/DatabaseWrapable.java
@@ -10,45 +10,42 @@ import java.util.Collection;
 /**
  * Classes which implement this interface can be used by the
  * {@link xyz.joestr.dbwrapper.DatabaseWrapper}.
+ *
  * @author Joel Strasser (joestr)
- * @version 0.1.0
+ * @version 0.1.2
  * @since 0.1.0
  */
 public interface DatabaseWrapable {
-    
+
     /**
      * Defines the name of the table in the database.
+     *
      * @return {@link java.lang.String} Name of the table
      * @author Joel Strasser (joestr)
-     * @version 0.1.0
+     * @version 0.1.2
      * @since 0.1.0
      */
     public String databaseTableName();
-    
+
     /**
      * Defines the name of the columns in the database.
-     * @return {@link java.util.Collection}&lt;{@link java.lang.String}&gt; Name of the columns in the database
+     *
+     * @return {@link java.util.Collection}&lt;{@link java.lang.String}&gt; Name
+     * of the columns in the database
      * @author Joel Strasser (joestr)
-     * @version 0.1.0
+     * @version 0.1.2
      * @since 0.1.0
      */
     public Collection<String> databaseColumnNames();
-    
+
     /**
      * Defines the name of the fields in the implementing class.
-     * @return {@link java.util.Collection}&lt;{@link java.lang.String}&gt; Name of the fields in the implementing class
+     *
+     * @return {@link java.util.Collection}&lt;{@link java.lang.String}&gt; Name
+     * of the fields in the implementing class
      * @author Joel Strasser (joestr)
-     * @version 0.1.0
+     * @version 0.1.2
      * @since 0.1.0
      */
     public Collection<String> classFieldNames();
-    
-    /**
-     * Defines the class of the fields in the implementing class.
-     * @return {@link java.util.Collection}&lt;{@link java.lang.Class}&gt; Class of the fields in the implementing class
-     * @author Joel Strasser (joestr)
-     * @version 0.1.0
-     * @since 0.1.0
-     */
-    public Collection<Class> classFieldClasses();
 }
