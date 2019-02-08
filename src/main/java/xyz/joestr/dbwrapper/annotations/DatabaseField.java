0dccbe0e45767648b0f43bf69d4b567c4b3834f4
joestr <strasser999@gmail.com>
2019-02-08 21:49:11 +0100

    added annotation based wrapping; added ""simple"" junit test;

---
+++ src/main/java/xyz/joestr/dbwrapper/annotations/DatabaseField.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xyz.joestr.dbwrapper.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface DatabaseField {
+
+    String name();
+}
---
+++ src/test/java/xyz/joestr/dbwrapper/test/DatabaseWrapperTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xyz.joestr.dbwrapper.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import xyz.joestr.dbwrapper.DatabaseConnectionHandler;
+
+/**
+ *
+ * @author Joel
+ */
+public class DatabaseWrapperTest {
+
+    @Test
+    public void notAPersitentConnectionByDefault() {
+        DatabaseConnectionHandler dCH = new DatabaseConnectionHandler("");
+
+        assertEquals(false, dCH.isPersistentConnection());
+    }
+}
